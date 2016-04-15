4b1f983ff15643ef0f0476c9d2fa05a6462cdd99
Anton Yurchenko <entony0@gmail.com>
2016-04-15 17:17:11 +0300

    add empty interface TextAnalyzer

---
+++ src/main/java/ru/lexmin/lexm_core/TextAnalyzer.java
@@ -0,0 +1,12 @@
+package ru.lexmin.lexm_core;
+
+/**
+ * Данный интерфейс описывает основной функционал анализа получаемого от
+ * пользователя текста
+ * 
+ * @author Anton Yurchenko
+ *
+ */
+public interface TextAnalyzer {
+
+}
