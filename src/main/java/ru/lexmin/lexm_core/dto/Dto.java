ea4421531b086daedd6bcd26631fa03b6a30c5a2
Anton Yurchenko <entony0@gmail.com>
2016-04-15 16:07:35 +0300

    up

---
+++ src/main/java/ru/lexmin/lexm_core/dto/Dto.java
@@ -0,0 +1,18 @@
+package ru.lexmin.lexm_core.dto;
+
+import java.io.Serializable;
+
+/**
+ * Класс является родителем для всех DTO классов проекта
+ * 
+ * @author Anton Yurchenko
+ *
+ */
+public class Dto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8833947379512255109L;
+
+}
